d17e688b84410558f461871d31bdd247813f1895
Adam Mikulis <adam.mikulis@gmail.com>
2023-12-07 17:52:39 -0700

    updated package model and added .xlsx data file

--- models/package.py
+++ models/package.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class PackageStatus(Enum):
     AT_HUB = "At-hub"
     IN_TRANSIT = "In-transit"
@@ -7,20 +8,21 @@ class PackageStatus(Enum):
 
 
 class Package:
-    def __init__(self, package_id=0, address="", city="", state="", zip=0, available_time=0, delivery_deadline=0,
-                 mass=0, required_truck=0, wrong_address=False, delivered_with=[], status=PackageStatus.AT_HUB):
+    def __init__(self, package_id=0, address_id=0, available_time=0, delivery_deadline=0,
+                 weight=0, required_truck=0, delivery_group_id=0, wrong_address=False):
+        # package_table columns
         self.package_id = package_id
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
+        self.address_id = address_id
         self.available_time = available_time
         self.delivery_deadline = delivery_deadline
-        self.mass = mass
+        self.weight = weight
         self.required_truck = required_truck
+        self.delivery_group_id = delivery_group_id
         self.wrong_address = wrong_address
-        self.delivered_with = delivered_with
-        self.status = status
+
+        # status variables
+        self.package_status = PackageStatus.AT_HUB
+        self.delivered_at = 0
 
     def set_package_id(self, number):
         self.package_id = number
@@ -28,8 +30,4 @@ class Package:
     def get_package_id(self):
         return self.package_id
 
-    def set_status(self, status):
-        self.status = status
 
-    def get_status(self):
-        return self.status.value
