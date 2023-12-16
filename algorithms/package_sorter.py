371092eab7e0c4a83540bf2a015e22cfc0175389
Adam Mikulis <adam.mikulis@gmail.com>
2023-12-16 09:46:15 -0700

    removing unneeded args, passing in just truck

--- algorithms/package_sorter.py
+++ algorithms/package_sorter.py
@@ -15,25 +15,22 @@ class PackageSorter:
     # This implements the pathing algorithm
     def load_truck(self, truck):
         last_address_id = '0'
-        total_truck_distance = 0.0
         while not truck.is_full() and self.package_id_list:
-            total_truck_distance = self.load_truck_if_package_id(total_truck_distance, truck)
+            self.load_truck_if_package_id(truck)
         # Sends trucks back to hub at the end of delivery
-        total_truck_distance = self.append_hub_address_id_at_end(last_address_id, total_truck_distance, truck)
-        return total_truck_distance
+        self.append_hub_address_id_at_end(last_address_id, truck)
 
-    def append_hub_address_id_at_end(self, last_address_id, total_truck_distance, truck):
+    def append_hub_address_id_at_end(self, last_address_id, truck):
         path_to_hub, distance_to_hub = self.algorithm.get_shortest_path(last_address_id, '0')
         # Remove the current address from the path to hub if it's the first path element
         if path_to_hub and path_to_hub[0] == last_address_id:
             path_to_hub = path_to_hub[1:]
         truck.truck_path_list.append(path_to_hub)
         truck.truck_distance_list.append(distance_to_hub)
-        total_truck_distance += distance_to_hub
-        return total_truck_distance
+        truck.total_miles_driven += distance_to_hub
 
     # Used in load_truck
-    def load_truck_if_package_id(self, total_truck_distance, truck):
+    def load_truck_if_package_id(self, truck):
         next_package_id, next_package_distance, next_path = self.get_next_closest_package_path(truck)
         package = self.package_hash_table.search(next_package_id)
         # Update the truck's path
@@ -48,14 +45,13 @@ class PackageSorter:
         truck.load_package(next_package_id)
         self.package_id_list.remove(next_package_id)
         truck.current_address_id = package.address_id
-        total_truck_distance += next_package_distance
-        return total_truck_distance
+        truck.total_miles_driven += next_package_distance
 
-    def print_truck_status(self, overall_distance, total_truck_distance, truck):
+    def print_truck_status(self, overall_distance, truck):
         print(f"Truck: {truck}\n"
               f"Path list: {truck.truck_path_list}\n"
               f"Distance list: {truck.truck_distance_list}\n"
-              f"Estimated Truck Distance: {total_truck_distance:.1f}\n"
+              f"Estimated Truck Distance: {truck.total_miles_driven:.1f}\n"
               f"Total Distance: {overall_distance:.1f}\n")
 
     # Iterates packing algorithm through all trucks
@@ -63,9 +59,9 @@ class PackageSorter:
         self.sort_special_packages_onto_trucks()
         overall_distance = 0.0
         for truck in self.truck_manager.trucks:
-            total_truck_distance = self.load_truck(truck)  # Algorithm implemented here
-            overall_distance += total_truck_distance
-            self.print_truck_status(overall_distance, total_truck_distance, truck)
+            self.load_truck(truck)  # Algorithm implemented here
+            overall_distance += truck.total_miles_driven
+            self.print_truck_status(overall_distance, truck)
 
     # Uses algorithm to recall shortest path
     def get_next_closest_package_path(self, truck):
